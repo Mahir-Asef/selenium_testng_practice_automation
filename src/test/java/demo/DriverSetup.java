4b08699f71435c0f801606af19ea5025ff00eda6
Mahir-Asef <mahirasef30@gmail.com>
2025-03-24 14:08:16 +0600

    few features added

--- src/test/java/demo/DriverSetup.java
+++ src/test/java/demo/DriverSetup.java
@@ -1,6 +1,8 @@
 package demo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -17,4 +19,32 @@ public class DriverSetup {
     public void closeBrowser(){
         driver.quit();
     }
+    public WebElement getElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public void loadPage(String url){
+        driver.get(url);
+    }
+
+    public void clickOnElement(By locator){
+        getElement(locator).click();
+    }
+
+    public void writeOnElement(By locator, String text){
+        getElement(locator).sendKeys(text);
+    }
+
+    public String getAttributeValue(By locator, String attributeName){
+        return getElement(locator).getAttribute(attributeName);
+    }
+
+    public Boolean isVisible(By locator){
+        return getElement(locator).isDisplayed();
+    }
+
+    public String getElementText(By locator){
+        return getElement(locator).getText();
+    }
+
 }
